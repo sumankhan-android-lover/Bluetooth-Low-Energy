3bf79c8bf8480561da068759e845e03806c70ac4
Suman khan <suman.khan@webskitters.com>
2022-09-05 00:44:12 +0530

    click bluetooth device

--- app/src/main/java/com/example/bluetoothconnectivity/MainActivity.kt
+++ app/src/main/java/com/example/bluetoothconnectivity/MainActivity.kt
@@ -8,21 +8,22 @@ import android.bluetooth.le.ScanCallback
 import android.bluetooth.le.ScanResult
 import android.content.Context
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.PermissionChecker
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.bluetoothconnectivity.databinding.ActivityMainBinding
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var scanDeviceAdapter: ScanDevicesAdapter
     private val TAG: String?= MainActivity::class.simpleName
     private lateinit var binding:ActivityMainBinding
     private var scanning = false
     private val handler = Handler()
     // Stops scanning after 10 seconds.
-    private val SCAN_PERIOD: Long = 10000
+    private val SCAN_PERIOD: Long = 20000
 
     private val bluetoothLeScanner: BluetoothLeScanner
         get() {
@@ -37,6 +38,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         bindActivity()
+        setupRecyclerViewAdapter()
     }
 
     private fun bindActivity() {
@@ -45,23 +47,48 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun setupRecyclerViewAdapter() {
+        scanDeviceAdapter = ScanDevicesAdapter(object :
+            ScanDevicesAdapter.RecyclerViewItemOnClickListener{
+            @SuppressLint("MissingPermission")
+            override fun onViewClick(position: ScanResult) {
+                Log.e(TAG, "onViewClick: item $position" )
+                bluetoothLeScanner.stopScan(leScanCallback)
+                binding.btnScan.text = "Start Scan"
+                //binding.status.text = "Status : Connecting ..."
+            }
+        })
+        with(binding.recycler) {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            this.adapter = scanDeviceAdapter
+        }
+    }
+
+
+
     @SuppressLint("MissingPermission")
     private fun scanDevices() {
-        if (!scanning) { // Stops scanning after a pre-defined scan period.
+        if (!scanning) {
+            // Stops scanning after a pre-defined scan period.
             handler.postDelayed({
                 scanning = false
                 bluetoothLeScanner.stopScan(leScanCallback)
+                binding.btnScan.text = "Start Scan"
             }, SCAN_PERIOD)
             scanning = true
             //PERMISSION COARSE LOCATION
             Log.d("ScanDeviceStart", "startScan()")
             when (PermissionChecker.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)) {
-                PackageManager.PERMISSION_GRANTED -> bluetoothLeScanner.startScan(leScanCallback)
+                PackageManager.PERMISSION_GRANTED -> {
+                    bluetoothLeScanner.startScan(leScanCallback)
+                    binding.btnScan.text = "Stop Scan"
+                }
                 else -> requestPermissions(arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), 1)
             }
         } else {
             scanning = false
             bluetoothLeScanner.stopScan(leScanCallback)
+            binding.btnScan.text = "Start Scan"
         }
     }
 
@@ -71,11 +98,13 @@ class MainActivity : AppCompatActivity() {
         override fun onScanResult(callbackType: Int, result: ScanResult) {
             super.onScanResult(callbackType, result)
             //TODO TEST TEST TEST
+            scanDeviceAdapter.addSingleItem(result)
             Log.d("ScanDeviceActivity", "leScanCallback >>")
             Log.d("ScanDeviceActivity", "onScanResult(): ${result?.device?.address} - ${result?.device?.name}")
         }
         override fun onBatchScanResults(results: MutableList<ScanResult>?) {
             super.onBatchScanResults(results)
+            results?.let { scanDeviceAdapter.setItems(it) }
             Log.d("DeviceListActivity","onBatchScanResults:${results.toString()}")
         }
 
@@ -84,12 +113,5 @@ class MainActivity : AppCompatActivity() {
             Log.d("DeviceListActivity", "onScanFailed: $errorCode")
         }
     }
-/*    private val leScanCallback: ScanCallback = object : ScanCallback() {
-        override fun onScanResult(callbackType: Int, result: ScanResult) {
-            super.onScanResult(callbackType, result)
-            Log.e(TAG, "onScanResult: ${result.device}" )
-//            leDeviceListAdapter.addDevice(result.device)
-//            leDeviceListAdapter.notifyDataSetChanged()
-        }
-    }*/
+
 }
--- app/src/main/java/com/example/bluetoothconnectivity/ScanDevicesAdapter.kt
+++ app/src/main/java/com/example/bluetoothconnectivity/ScanDevicesAdapter.kt
@@ -1,4 +1,83 @@
 package com.example.bluetoothconnectivity
 
-class ScanDevicesAdapter  {
+import android.annotation.SuppressLint
+import android.bluetooth.le.ScanResult
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.bluetoothconnectivity.databinding.ItemScanDeviceBinding
+
+class ScanDevicesAdapter(val listener: RecyclerViewItemOnClickListener) : RecyclerView.Adapter<ScanDevicesAdapter.ScanResultVh>() {
+    companion object{
+        private const val TAG: String = "ScanDevicesAdapter"
+    }
+    private var onItemClick: ((ScanResult) -> Unit)? = null
+    private var itemsList: MutableList<ScanResult> = arrayListOf()
+
+    @SuppressLint("MissingPermission")
+    fun setItems(mutableList: MutableList<ScanResult>) {
+        if (mutableList != itemsList) {
+            mutableList.forEach{ data ->
+                if (data.device.name != null) itemsList.add(data)
+                //mutableList.add()
+            }
+            //itemsList = mutableList
+            notifyDataSetChanged()
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    fun addSingleItem(item: ScanResult) {
+        /**
+         * In this particular case the data coming in may be duplicate. So check that only unique
+         * elements are admitted: the device Id + device name should create a unique pair.
+         * removeIf requires API level 24, so using removeAll here. But feel free to use any of
+         * a number of options. Remove the previous element so to keep the latest timestamp
+         */
+        itemsList.removeAll {
+            it.device.name == item.device.name && it.device.address == item.device.address
+        }
+        if (item.device.name != null) itemsList.add(item)
+        notifyDataSetChanged()
+    }
+
+    private fun getItem(position: Int): ScanResult? = if (itemsList.isEmpty()) null else itemsList[position]
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ScanDevicesAdapter.ScanResultVh {
+        val binding = ItemScanDeviceBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ScanResultVh(binding/*,onClickListener*/)
+    }
+
+    override fun onBindViewHolder(holder: ScanDevicesAdapter.ScanResultVh, position: Int) {
+        Log.e(TAG, "onBindViewHolder: called for position $position")
+        holder.bind(getItem(position))
+    }
+
+    override fun getItemCount() = itemsList.size
+
+    inner class ScanResultVh(private val binding: ItemScanDeviceBinding/*, private val onClickListener: ((device: ScanResult) -> Unit)*/) :
+        RecyclerView.ViewHolder(binding.root) {
+        @SuppressLint("MissingPermission")
+        fun bind(item: ScanResult?) {
+            item?.let {
+                binding.deviceName.text = it.device.name
+                binding.deviceAddress.text = it.device.address
+                //binding.lastSeen.text = it.timestampNanos.toString()
+                binding.root.setOnClickListener {
+                    listener.onViewClick(item)
+                   // onItemClick?.invoke(itemsList[adapterPosition])
+                }
+            }
+
+        }
+    }
+
+    interface RecyclerViewItemOnClickListener {
+        fun onViewClick(position: ScanResult)
+    }
+
 }
